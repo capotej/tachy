1361e76969cfe0fa6e953aae241bd255453e866d
Julio Capote <jcapote@gmail.com>
2011-07-10 19:55:44 -0700

    logging to redis

--- server.rb
+++ server.rb
@@ -1,5 +1,6 @@
-require 'eventmachine'
-require 'evma_httpserver'
+require 'bundler'
+Bundler.require
+require 'cgi'
 
 class TachyServer < EM::Connection
   include EM::HttpServer
@@ -21,15 +22,23 @@ class TachyServer < EM::Connection
     #   @http_query_string
     #   @http_post_content
     #   @http_headers
-    
+    puts "#{Time.now.to_f} accessing #{@http_request_uri}"
+    if @http_request_uri == "/log"
+      params = CGI.parse(@http_post_content)
+      p params["name"]
+      $redis.lpush(params["name"][0], "#{Time.now.to_f}:#{params["data"][0]}")
+      $redis.ltrim(params["name"][0], 0, 100_000)
+    end
+
     response = EM::DelegatedHttpResponse.new(self)
     response.status = 200
-    response.content_type 'text/html'
-    response.content = '<center><h1>Hi there</h1></center>'
+    response.content_type 'text/plain'
+    response.content = 'OK'
     response.send_response
   end
 end
 
 EM.run{
+  $redis = EM::Protocols::Redis.connect
   EM.start_server '0.0.0.0', 8080, TachyServer
 }
