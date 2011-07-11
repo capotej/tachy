2017e70a90e37caacd171d09012f8c808cadc027
Julio Capote <jcapote@gmail.com>
2011-07-10 20:42:41 -0700

    interface

--- server.rb
+++ server.rb
@@ -1,6 +1,9 @@
 require 'bundler'
 Bundler.require
 require 'cgi'
+require 'json'
+
+WINDOW_SIZE = 1000
 
 class TachyServer < EM::Connection
   include EM::HttpServer
@@ -23,18 +26,45 @@ class TachyServer < EM::Connection
     #   @http_post_content
     #   @http_headers
     puts "#{Time.now.to_f} accessing #{@http_request_uri}"
-    if @http_request_uri == "/log"
+    if @http_request_uri == "/api/log"
       params = CGI.parse(@http_post_content)
-      p params["name"]
+      $redis.sadd('tachy_lists', params["name"][0])
       $redis.lpush(params["name"][0], "#{Time.now.to_f}:#{params["data"][0]}")
-      $redis.ltrim(params["name"][0], 0, 100_000)
+      $redis.ltrim(params["name"][0], 0, WINDOW_SIZE)
+      response = EM::DelegatedHttpResponse.new(self)
+      response.status = 200
+      response.content_type 'text/plain'
+      response.content = 'OK'
+      response.send_response
+    end
+    if @http_request_uri == "/api/funcs"
+      $redis.smembers('tachy_lists') do |members|
+        response = EM::DelegatedHttpResponse.new(self)
+        response.status = 200
+        response.content_type 'application/json'
+        response.content = members.to_json
+        response.send_response
+      end
+    end
+    if @http_request_uri.include?("/api/view")
+      func_name = @http_request_uri.split("/").last.gsub('/','')
+      $redis.lrange(func_name, 0, WINDOW_SIZE) do |range|
+        response = EM::DelegatedHttpResponse.new(self)
+        response.status = 200
+        response.content_type 'application/json'
+        response.content = range.to_json
+        response.send_response
+      end
+    end
+    if @http_request_uri == "/dashboard"
+      response = EM::DelegatedHttpResponse.new(self)
+      response.status = 200
+      response.content_type 'text/html'
+      response.content = File.read('index.html')
+      response.send_response
     end
 
-    response = EM::DelegatedHttpResponse.new(self)
-    response.status = 200
-    response.content_type 'text/plain'
-    response.content = 'OK'
-    response.send_response
+
   end
 end
 
--- test.rb
+++ test.rb
@@ -3,7 +3,7 @@ require 'rest-client'
 
 loop do
   st = Time.now.to_f
-  RestClient.post 'http://localhost:8080/log', { :name => 'test_func', :data => rand(112312312) }
+  RestClient.post 'http://localhost:8080/api/log', { :name => 'test_func', :data => rand(112312312) }
   et = Time.now.to_f
   puts st.to_f - et.to_f
 end
